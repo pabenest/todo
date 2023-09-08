e291dda55ca32e19d4a76f25381bf772e1292f61
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2023-09-08 11:50:26 +0200

    chore: init project

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { input } from '@inquirer/prompts';
+
+async function main() {
+    const answer = await input({ message: 'Enter your name' });
+
+    console.log(`Hello ${answer}`);
+}
+
+main();
